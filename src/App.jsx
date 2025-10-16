f3244a737f7fbd1c47b2d731d3ae9dc405ce8a95
najmiddininomov <Inomovnajmiddin737@gmail.com>
2025-10-16 09:41:37 +0500

    Fixed CSS import and rebuild

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
-import "./app.css";
+import "./App.css";
 import Footer from "./components/Footer/Footer";
 import About from "./pages/About/About";
 import Nature from "./pages/Nature/Nature";
